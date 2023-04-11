1fb20f780f5d81f59a359264b948bfac955dcdf9
jge4786 <jge1584@gmail.com>
2023-04-11 14:19:56 +0900

    cxvc

--- Mytube/SearchViewController.swift
+++ Mytube/SearchViewController.swift
@@ -11,7 +11,7 @@ final class SearchViewController: UIViewController {
     @IBOutlet weak var contentScrollView: UIScrollView!
     @IBOutlet weak var goBackButton: UIButton!
     
-    func abc(){}
+    func abc333(){}
 
     override func viewDidLoad() {
         super.viewDidLoad()
